bd02d5222e38602c3a4cb9e87aa92541d02d4969
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2018-02-15 14:53:44 +0200

    draft prie slider added

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 //= jquery.formstyler.min.js
+//= jquery-ui.min.js
 $(document).ready(function() {
   // tabs
   $('.tabs a').on('click', function(event) {
@@ -21,5 +22,20 @@ $(document).ready(function() {
    });
 
    $('.styler').styler();
+
+   // price range slider
+   $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] );
+        $( "#amount2" ).val( "$" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount2" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+
 
 });
